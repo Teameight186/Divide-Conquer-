64e9c6a77ea15b7526f9dc2d8e641f57b3156bbc
SaimsNO <blitztownone@gmail.com>
2024-05-14 10:28:46 +0300

    fix: structure

--- algorithms cpp/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/vs/Divide-Conquer-/algorithms cpp
-# Build directory: D:/projects/vs/Divide-Conquer-/algorithms cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
