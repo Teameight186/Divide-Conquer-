a86edf6822b05f1651d8064bf8f117e1ecc9b7b9
SaimsNO <blitztownone@gmail.com>
2024-05-14 09:53:13 +0300

    create: structure project

---
+++ algorithms cpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/projects/vs/Divide-Conquer-/algorithms cpp
+# Build directory: D:/projects/vs/Divide-Conquer-/algorithms cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
